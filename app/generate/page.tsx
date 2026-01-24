8e0a4b0d1d8cef15f16a5657a6b2f37525ffcf9f
Jivan Gursal <jivangursal23@gmail.com>
2026-01-24 15:10:15 +0530

    error fix

---
+++ app/generate/page.tsx
@@ -0,0 +1,9 @@
+export default function GeneratePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">
+        Generate Page
+      </h1>
+    </main>
+  );
+}
